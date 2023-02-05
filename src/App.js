d4a83ff57e977fa8e8e21ac1f9e4e18ed3ad7630
Stephen Pflug <stephen@pflug.us>
2023-02-05 09:08:26 -0600

    Implementing Child to Parent Communication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import searchBar from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
+
 function App() {
+    const handleSubmit = (term) => {
+        console.log(`Do a search with ${term}.`);
+    }
+
     return(
         <div>
-            App
+            <SearchBar onSubmit={handleSubmit} />
         </div>
     );
 }
