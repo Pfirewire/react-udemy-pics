4db7b5db5962878e824cedb0febec3bada03e291
Stephen Pflug <stephen@pflug.us>
2023-02-05 08:49:50 -0600

    Making an HTTP Request

--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import UNSPLASH_ACCESS_KEY from "./keys";
 
-const searchImages = () => {
-    axios.get('https://api.unsplash.com/search/photos', {
+const searchImages = async () => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID ' + UNSPLASH_ACCESS_KEY
         },
         params: {
-
+            query: 'cars'
         }
     });
+
+    console.log(response);
+    return response;
 }
+
+export default searchImages;
