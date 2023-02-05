8d89a4dafe3e1997945c06cf2509bea788ab2779
Stephen Pflug <stephen@pflug.us>
2023-02-05 09:23:56 -0600

    Handling Input Elements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,21 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+    const [query, setQuery] = useState('');
+
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log("test");
-    }
+        onSubmit('cars');
+    };
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    };
 
     return(
         <div>
             <form onSubmit={handleFormSubmit}>
-                <input />
+                <input value={query} onChange={handleChange} />
             </form>
         </div>
     );
